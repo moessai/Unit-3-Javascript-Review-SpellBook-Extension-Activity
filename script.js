78e6bcf15b7f8d85471084f3e259ef5a6634f490
moessai <123699687+moessai@users.noreply.github.com>
2023-01-26 14:08:25 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+let rabbit=document.querySelector (".rabbit");
+let spell1button=document.querySelector (".spell-onebutton");
+let spell2=document.querySelector (".header-2");
+let spell2button=document.querySelector (".spell-twobutton");
+let spell3button=document.querySelector (".spell-threebutton");
+let body=document.querySelector ("body");
+
+spell1button.onclick=function(){
+rabbit.style.display="block";};
+
+
+
+
+
